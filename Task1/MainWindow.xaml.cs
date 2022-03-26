b22f498dc7ef546d975cb8df2c5153660eafb266
Denis Mikhaylov <dnmh@yandex.ru>
2022-03-26 15:10:08 +0700

    Добавьте файлы проекта.

---
+++ Task1/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SolidColorBrush defaultFontColor = Brushes.Black;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Bold_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.FontWeight = (textBox.FontWeight != FontWeights.Bold) ? FontWeights.Bold : FontWeights.Normal;
+            }
+        }
+
+        private void Button_Italic_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.FontStyle = (textBox.FontStyle != FontStyles.Italic) ? FontStyles.Italic : FontStyles.Normal;
+            }
+        }
+
+        private void Button_Underline_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.TextDecorations = (textBox.TextDecorations != TextDecorations.Underline) ? TextDecorations.Underline : null;
+            }
+        }
+
+        private void RadioButton_Black_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = defaultFontColor;
+            }
+        }
+
+        private void RadioButton_Red_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = Brushes.Red;
+            }
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void SetLightThemeExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            defaultFontColor = Brushes.Black;
+            ChangeTheme("Light.xaml");
+        }
+
+        private void SetDarkThemeExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            defaultFontColor = Brushes.White;
+            ChangeTheme("Dark.xaml");
+        }
+
+        private void ChangeTheme(String ThemeDictionary)
+        {
+            Uri uri = new Uri(ThemeDictionary, UriKind.Relative);
+            ResourceDictionary resource = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resource);
+            if (textBox.Foreground != Brushes.Red)
+            {
+                textBox.Foreground = defaultFontColor;
+            }
+        }
+    }
+}
---
+++ Task1/TextEditorCommands.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Task1
+{
+    class TextEditorCommands
+    {
+        public static RoutedUICommand Exit { get; set; }
+        public static RoutedUICommand SetLightTheme { get; set; }
+        public static RoutedUICommand SetDarkTheme { get; set; }
+
+        static TextEditorCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.T, ModifierKeys.Control, "Ctrl+T"));
+            Exit = new RoutedUICommand("Выход", "Exit", typeof(TextEditorCommands), inputs);
+
+            SetLightTheme = new RoutedUICommand("Светлая тема", "SetLightTheme", typeof(TextEditorCommands));
+
+            SetDarkTheme = new RoutedUICommand("Тёмная тема", "SetDarkTheme", typeof(TextEditorCommands));
+        }
+    }
+}
